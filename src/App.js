2de01a08557e201df53af6774ed810c10c83441b
Emil <eesenjanov@gmail.com>
2022-05-10 23:29:53 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import AuthPage from "./pages/AuthPage";
 import CartContextProvider from "./contexts/CartContextProvider"
 import AuthContextProvider from "./contexts/AuthContextProvider";
 import Footer from "./components/Footer/Footer";
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
             <Navbar />
-            <AuthPage />
+            <MainRoutes />
             <Footer />
       </AuthContextProvider>
     </div>
