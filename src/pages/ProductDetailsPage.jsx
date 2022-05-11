16a889f6a0628ec01b3b0fab9007fb8479bbe3fe
Emil <eesenjanov@gmail.com>
2022-05-11 22:05:53 +0600

    added product details page

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProductDetails from '../components/Product/ProductDetails';
 
 const ProductDetailsPage = () => {
   return (
     <div>
       Product Details
+      <ProductDetails />
     </div>
   );
 };
